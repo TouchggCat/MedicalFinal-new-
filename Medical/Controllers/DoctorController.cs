935a74280df2a513f45a071fbd9fd45407b1aa86
Lamenseal <kartd0079818@gmail.com>
2022-07-22 11:46:14 +0800

    醫生總覽更新頁面排版 新增按鈕 推薦醫生(UI模型)推薦和複選功能還沒做好 可以單選搜尋 醫生前後台部分顯示改用AJAX

--- Medical/Controllers/DoctorController.cs
+++ Medical/Controllers/DoctorController.cs
@@ -16,7 +16,6 @@ namespace Medical.Controllers
     {
         private IWebHostEnvironment _enviroment;
         private readonly MedicalContext _db;
-        //int role = 2;                        //權限
         public DoctorController(IWebHostEnvironment p, MedicalContext db)
         {
             _enviroment = p;
@@ -24,7 +23,6 @@ namespace Medical.Controllers
         }
         public IActionResult Index(CKeyWordViewModel vModel)     //醫生後台主頁
         {
-
             IEnumerable<Doctor> datas = null;
             if (string.IsNullOrEmpty(vModel.txtKeyword))
             {
@@ -67,39 +65,86 @@ namespace Medical.Controllers
             IEnumerable<Doctor> datas = null;
             if (string.IsNullOrEmpty(vModel.txtKeyword))
             {
-                datas = from t in _db.Doctors
-                        join d in _db.Departments on t.DepartmentId equals d.DepartmentId
-                        select t;
+                datas = from doc in _db.Doctors
+                        join d in _db.Departments on doc.DepartmentId equals d.DepartmentId
+                        select doc;
             }
             else
             {
-                datas = _db.Doctors.Where(t => t.DoctorName.Contains(vModel.txtKeyword) ||
-                t.Education.Contains(vModel.txtKeyword) || t.JobTitle.Contains(vModel.txtKeyword));
+                datas = _db.Doctors.Where(t => t.DoctorName.Contains(vModel.txtKeyword));
+                //||
+                //t.Education.Contains(vModel.txtKeyword) || t.JobTitle.Contains(vModel.txtKeyword));
             }
             return View(datas);
         }
-        public IActionResult getDep()    //開發中 Department選單
+        //public IActionResult getDep()    //開發中 Department選單
+        //{
+        //    var dep = _db.Departments.Select(a => a.DeptName).Distinct();
+        //    return Json(dep);
+        //}
+        //public IActionResult getDoc(string depName)   //開發中 Department選單
+        //{
+        //    Department depN = _db.Departments.FirstOrDefault(b => b.DeptName == depName);
+        //    var doc = _db.Doctors.Where(d => d.DepartmentId == depN.DepartmentId).Select(b => b.DoctorName).Distinct();
+        //    return Json(doc);
+        //}
+        public IActionResult getTreatment(int? dcID)
         {
-            var dep = _db.Departments.Select(a => a.DeptName).Distinct();
-            return Json(dep);
+            var trt = from a in _db.Treatments
+                      join b in _db.TreatmentDetails on a.TreatmentDetailId equals b.TreatmentDetailId
+                      where a.DoctorId == dcID
+                      select b.TreatmentDetail1;
+            
+            return Json(trt);
         }
-        public IActionResult getDoc(string depName)   //開發中 Department選單
+        //public IActionResult getTreatmentDetail(int? trtID)
+        //{
+        //    Treatment trt = _db.Treatments.FirstOrDefault(b => b.TreatmentId == trtID);
+        //    var trtD = _db.TreatmentDetails.Where(d => d.TreatmentDetailId == trt.TreatmentDetailId).Select(b => b.TreatmentDetail1).Distinct();
+        //    return Json(trtD);
+        //}
+        public IActionResult Detail(int? id)
         {
-            Department depN = _db.Departments.FirstOrDefault(b => b.DeptName == depName);
-            var doc = _db.Doctors.Where(d => d.DepartmentId == depN.DepartmentId).Select(b => b.DoctorName).Distinct();
-            return Json(doc);
+            if (id == null)
+            {
+                return RedirectToAction("ListTest");
+            }
+            CDoctorDetailViewModel prod = new CDoctorDetailViewModel();
+            Doctor DD = _db.Doctors.FirstOrDefault(t => t.DoctorId == id);
+            Experience exp = _db.Experiences.FirstOrDefault(t => t.DoctorId == id);
+            Treatment trt = _db.Treatments.FirstOrDefault(t => t.DoctorId == id);
+            
+            prod.doctor = DD;
+            if (DD.DepartmentId != null)
+                prod.department = _db.Departments.FirstOrDefault(t => t.DepartmentId == prod.doctor.DepartmentId);
+            if (exp != null)
+                prod.experience = _db.Experiences.FirstOrDefault(t => t.DoctorId == prod.doctor.DoctorId);
+            if (trt != null)
+            {
+                prod.treatment = trt;
+                TreatmentDetail trtD = _db.TreatmentDetails.FirstOrDefault(t => t.TreatmentDetailId == trt.TreatmentDetailId);
+                if (trtD != null)
+                prod.treatmentDetail = _db.TreatmentDetails.FirstOrDefault(t => t.TreatmentDetailId == prod.treatment.TreatmentDetailId);
+            }
+            
+            if (prod == null)
+                return RedirectToAction("Index");
+            
+            return View(prod);
         }
-
-
+        public IActionResult SugDoc() {
+            
+            return View();
+        }
         //==========冠名==========
         //瀏覽醫生評論
-        
+        //id寫死
         public IActionResult RatingDoctorpartail(int id)
         {
             ViewBag.name = _db.RatingDoctors.Where(a => a.DoctorId == id).Select(a => a.Doctor.DoctorName).FirstOrDefault();
-            ViewBag.count = _db.RatingDoctors.Where(a => a.DoctorId == id).Where(a => a.Shade == false || a.Shade == null).Select(a=>a.Rating).Count();
+            ViewBag.count = _db.RatingDoctors.Where(a => a.DoctorId == id).Where(a => a.Shade == false).Select(a=>a.Rating).Count();
 
-            var result = _db.RatingDoctors.Where(a => a.DoctorId == id).Where(a => a.Shade == false|| a.Shade == null)
+            var result = _db.RatingDoctors.Where(a => a.DoctorId == id).Where(a => a.Shade == false)
                 .Include(a => a.Doctor)
                 .Include(a => a.RatingType)
                 .ToList();
--- Medical/Models/City.cs
+++ Medical/Models/City.cs
@@ -9,12 +9,14 @@ namespace Medical.Models
     {
         public City()
         {
+            Members = new HashSet<Member>();
             Orders = new HashSet<Order>();
         }
 
         public int CityId { get; set; }
         public string CityName { get; set; }
 
+        public virtual ICollection<Member> Members { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
     }
 }
