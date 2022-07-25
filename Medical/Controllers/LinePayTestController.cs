4d6ff9f036ee21147a0c8ec63fc3652ee45e5630
AlexWang <wanbo841019@gmail.com>
2022-07-25 16:44:47 +0800

    0725下午

--- Medical/Controllers/LinePayTestController.cs
+++ Medical/Controllers/LinePayTestController.cs
@@ -2,16 +2,113 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using System.Web.Helpers;
+using static Medical.CoreLinePay;
+using Newtonsoft.Json;
+using System.Text;
+using Medical.ViewModel;
 
 namespace Medical.Controllers
 {
     public class LinePayTestController : Controller
     {
-        public IActionResult Index()
+        public IActionResult test()
         {
             return View();
         }
+        //public async Task<IActionResult> OnPostSubmitAsync()
+        //{
+        //    var result = await RequestLinePayAsync(213);
+        //    return Redirect(result.info.paymentUrl.web);
+        //}
 
+        public async Task RequestLinePayAsync(int amount)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                //Setting
+                var channelId = "1657329218";
+                var channelSecret = "d5a97c039996d5b4c7dc0b4a3f48b784";
+                var baseUri = "https://sandbox-api-pay.line.me";
+                var apiUri = "/v3/payments/request";
+                string orderId = Guid.NewGuid().ToString();
+
+                //設定 Body
+                var requsetBody = new LinePayRequest()
+                {
+                    amount = amount,
+                    currency = "TWD",
+                    orderId = Guid.NewGuid().ToString(),
+                    redirectUrls = new Redirecturls()
+                    {
+                        confirmUrl = "https://localhost:44302/LinePayTest/confirm"
+                    },
+                    packages = new List<Package>()
+                    {
+                        new Package(){ id="package-1",name = "MedicalLinePay",
+                            amount = amount,
+                            products = new List<Product2>(){
+
+                                new Product2(){
+                                    id = "prod1",
+                                    name = "testProd1",
+                                    //imageUrl=""
+                                    quantity = 1,
+                                    price = amount
+                                }
+                            }
+                        }
+                    }
+                };
+                var body = JsonConvert.SerializeObject(requsetBody);
+                //加密
+                string Signature = HasLinePayRequest(channelSecret,apiUri,body,orderId,channelSecret);
+
+                //設定 Header
+                httpClient.DefaultRequestHeaders.Add("X-LINE-ChannelId", channelId);
+                httpClient.DefaultRequestHeaders.Add("X-LINE-ChannelSecret", channelSecret);
+                httpClient.DefaultRequestHeaders.Add("X-LINE-Authorization-Nonce", orderId);
+                httpClient.DefaultRequestHeaders.Add("X-LINE-Authorization", Signature);
+
+                var content = new StringContent(body, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync(baseUri + apiUri, content);
+                var result = await response.Content.ReadAsStringAsync();
+
+                JsonConvert.DeserializeObject<LinePayRequest>(result);
+            }
+
+        }
+
+
+        private static string HasLinePayRequest(string channelSecret, string apiUrl,
+            string body, string orderId, string key)
+        {
+            var request = channelSecret + apiUrl + body + orderId;
+
+            key = key ?? "";
+
+            var encoding = new System.Text.UTF8Encoding();
+
+            byte[] keyByte = encoding.GetBytes(key);
+            byte[] messageBytes = encoding.GetBytes(request);
+
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+
+                return Convert.ToBase64String(hashmessage);
+            }
+        }
+
+        [HttpGet("Confirm")]
+        public async Task<IActionResult>Confirm([FromQuery] string orderId,[FromQuery]string transactionId)
+        {
+            return Ok();
+        }
+
+        
     }
 }
--- Medical/CoreLinePay.cs
+++ Medical/CoreLinePay.cs
@@ -7,86 +7,75 @@ namespace Medical
 {
     public class CoreLinePay
     {
-        static void Main(string[] args)
+        public class LinePayRequest
+
         {
-            string channelSecret = "d5a97c039996d5b4c7dc0b4a3f48b784";
-            string requestUri = "/v3/payments/request";
+            public int amount { get; set; }
 
-            LineForm json = new LineForm
-            {
-                Amount = 4000,
-                Currency = "TWD",
-                OrderId = Guid.NewGuid().ToString(),
-                Packages = new List<Package>
-                {
-                    new Package
-                    {
-                        Id = "2",
-                        Amount = 5000,
-                        Name = "測試",
-                        Products = new List<Product>
-                        {
-                            new Product
-                            {
-                                Name = "測試商品",
-                                Quantity = 2,
-                                Price = 2500
-                            }
-                        }
-                    }
-                },
-                RedirectUrls = new RedirectUrls
-                {
-                    ConfirmUrl = "https://6ddcf789.ngrok.io/confitmUrl",
-                    CancelUrl = "https://6ddcf789.ngrok.io/cancelUrl"
-                }
+            public string currency { get; set; }
 
-            };
-        }
+            public string orderId { get; set; }
 
-        public class Product
-        {
-            public string Name { get; set; }
+            public List<Package> packages { get; set; }
 
-            public int Quantity { get; set; }
+            public Options options { get; set; }
 
-            public int Price { get; set; }
+            public Redirecturls redirectUrls { get; set; }
+
         }
 
         public class Package
+
         {
-            public string Id { get; set; }
 
-            public int Amount { get; set; }
+            public string id { get; set; }
 
-            public string Name { get; set; }
+            public int amount { get; set; }
 
-            public List<Product> Products { get; set; }
+            public string name { get; set; }
+
+            public List<Product2> products { get; set; }
+
         }
 
-        public class RedirectUrls
-        {
-            public string ConfirmUrl { get; set; }
+        public class Product2
 
-            public string CancelUrl { get; set; }
+        {
+            public string id { get; set; }
+
+            public string name { get; set; }
+
+            public string imageUrl { get; set; }
+
+            public int quantity { get; set; }
+
+            public int price { get; set; }
+
         }
 
-        public class LineForm
+        public class Options
+
         {
-            //[JsonPropertyName("amount")]
-            public int Amount { get; set; }
+            public Payment payment { get; set; }
 
-            //[JsonPropertyName("currency")]
-            public string Currency { get; set; }
+        }
 
-            //[JsonPropertyName("orderId")]
-            public string OrderId { get; set; }
+        public class Payment
 
-            //[JsonPropertyName("packages")]
-            public List<Package> Packages { get; set; }
+        {
 
-            //[JsonPropertyName("redirectUrls")]
-            public RedirectUrls RedirectUrls { get; set; }
+            public bool capture { get; set; }
+
+        }
+
+        public class Redirecturls
+
+        {
+
+            public string confirmUrl { get; set; }
+
+            public string cancelUrl { get; set; }
+
         }
     }
 }
