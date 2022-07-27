728e28f41def105cafe4940215608207ad124e5c
sd7822735 <sd7822735@outlook.com>
2022-07-27 10:19:52 +0800

    調整前、後台 所以項目的修整跟排版微調整

--- Medical/wwwroot/js/chat.js
+++ Medical/wwwroot/js/chat.js
@@ -10,34 +10,28 @@ connection.on("ReceiveMessage", function (user, message) {
     var MSN = document.querySelector("#MSN");
     var time = moment(Date.now()).format("LTS")
     if (user == `客服`) {
-        console.log(user);
         $("#MSN").append(`<div class="direct-chat-msg text-md-end">
                                 <div class="direct-chat-infos clearfix">
-                                    <span class="direct-chat-name float-left" id="name_player1">客服</span>
-                                    <span class="direct-chat-timestamp float-right" id="time_player1">${time}</span>
+                                    <span class="direct-chat-name float-left" id="name_player1">《客服》</span>
+                                    <span class="direct-chat-timestamp float-right" id="time_player1">(${time})</span>
                                 </div>
                                 <img class="direct-chat-img" src="../img/客服.png" alt="Message User Image" style="border-radius:50%">
                             <div class="direct-chat-text">
-                                <ul id="message_player1">
-                                    <li>${message}</li>
-                                </ul>
+                                <span class="text-bold">${message}</span>
                             </div>
                             </div>`)
 
     }
     else{
-        console.log(user);
-        $("#MSN").append(`<div class="direct-chat-msg text-md-end">
+        $("#MSN").append(`<div class="direct-chat-msg">
                         <div class="direct-chat-infos clearfix">
-                            <span class="direct-chat-name float-left" id="name_player1">訪客</span>
-                            <span class="direct-chat-timestamp float-right" id="time_player1">${time}</span>
+                            <span class="direct-chat-name float-left" id="name_player1">《訪客》</span>
+                            <span class="direct-chat-timestamp float-right" id="time_player1">(${time})</span>
                         </div>
                         <img class="direct-chat-img" src="../img/訪客.jpg" alt="Message User Image" style="border-radius:50%">
                        
                     <div class="direct-chat-text">
-                        <ul id="message_player1">
-                                <li>${message}</li>
-                        </ul>
+                        <span class="text-bold">${message}</span>
                     </div>
                     </div>`)
     }
