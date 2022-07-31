af9ef5a99ae314ea700e5f76935efe429e516041
Hank Hsu <bearbo888@yahoo.com.tw>
2022-07-31 11:17:40 +0800

    新增門診修正一次多筆以及修改介面跟防呆 & 叫號改用SignalR

---
+++ Medical/Hubs/DashboardHub.cs
@@ -0,0 +1,23 @@
+﻿using Medical.Controllers;
+using Medical.Models;
+using Medical.Repo;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Medical.Hubs
+{
+    public class DashboardHub : Hub
+    {
+        private readonly MedicalContext _medicalContext;
+        public DashboardHub(MedicalContext medicalContext)
+        {
+            _medicalContext = new MedicalContext();
+        }
+        public async Task SendProducts()
+        {
+            var products = _medicalContext.Products;
+            await Clients.All.SendAsync("ReceivedProducts", products);
+        }
+    }
+}
